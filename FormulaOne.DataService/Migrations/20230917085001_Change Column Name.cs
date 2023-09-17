64468adf18042cb5703428f2d7c4e72f7a6ce442
fahama khan <fahamakhan990@gmail.com>
2023-09-17 13:57:24 +0500

    create second migraion for #33467ebd

---
+++ FormulaOne.DataService/Migrations/20230917085001_Change Column Name.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FormulaOne.DataService.Migrations
+{
+    public partial class ChangeColumnName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FatestLap",
+                table: "Achievements",
+                newName: "FastestLap");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FastestLap",
+                table: "Achievements",
+                newName: "FatestLap");
+        }
+    }
+}
